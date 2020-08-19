e9a28cba9b37ffb97e97906d79a10310fb259c6c
RandyLiu6410 <wert6410@gmail.com>
2020-08-19 23:13:27 +0800

    Initial

---
+++ NBATikTok/NBATikTok/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  NBATikTok
+//
+//  Created by RandyLiu on 19/8/2020.
+//  Copyright © 2020 h-tamader-team/RandyLiu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
---
+++ NBATikTok/NBATikTok/PreviewView.swift
@@ -0,0 +1,33 @@
+//
+//  PreviewView.swift
+//  NBATikTok
+//
+//  Created by RandyLiu on 19/8/2020.
+//  Copyright © 2020 h-tamader-team/RandyLiu. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+        }
+        return layer
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+}
